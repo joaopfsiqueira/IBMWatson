91219b2e39f6144570c769b053f51d86ffc0a9d1
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2021-06-16 19:06:37 -0300

    Alterações no readme, gitignore e limpeza de cod.

--- back/Server/antigo.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-    const db = require('./db');
-    await db.insertComment({Comentario: "O rato roeu a rica roupa do rei de Roma!" });
-    const Comentarios = await db.selectComments();
-    console.log(Comentarios)
-    
-})();
-
--- front/assets/js/script.js
+++ front/assets/js/script.js
@@ -1,10 +1,10 @@
 const lista = document.getElementById("lista");
 fetch('http://localhost:3000/')
-.then(response => response.json())
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
     let allItems = ""
     data.map(Comentarios => {
-        allItems += `
+      allItems += `
     <div class="listaComentariosdivh2">
     <span class="spanComentarios">${Comentarios.Comentario}</span>
     <button onclick="playAudio(${Comentarios.idComentario})">Ouvir</button>
@@ -13,46 +13,46 @@ fetch('http://localhost:3000/')
       </audio>
     </div>
 `
-})
+    })
     lista.innerHTML = allItems;
-})
-.catch(err => console.log(err))
-;
+  })
+  .catch(err => console.log(err))
+  ;
 
 
 
 
-    const btnCadastrar = document.getElementById("btnSubmit");
-    btnCadastrar.addEventListener("click", async() => {
-        const txtArea = document.getElementById("txtArea").value;
-        if (!txtArea) {
-          alert("Campo vazio! Preencha com alguma informação.")
-        } else {
-          const response = await sendComment(txtArea)
-          const allComments = await response.json()
-          let allItems = ""
-          allComments.map(Comentarios => {
-              allItems += `
+const btnCadastrar = document.getElementById("btnSubmit");
+btnCadastrar.addEventListener("click", async () => {
+  const txtArea = document.getElementById("txtArea").value;
+  if (!txtArea) {
+    alert("Campo vazio! Preencha com alguma informação.")
+  } else {
+    const response = await sendComment(txtArea)
+    const allComments = await response.json()
+    let allItems = ""
+    allComments.map(Comentarios => {
+      allItems += `
           <div class="listaComentariosdivh2">
           <span class="spanComentarios">${Comentarios.Comentario}</span>
           <button type="submit">Ouvir</button>
           </div>
       `
-      })
-          lista.innerHTML = allItems;
-        }
     })
+    lista.innerHTML = allItems;
+  }
+})
 
-    async function sendComment(txtArea) {
-    return await fetch('http://localhost:3000/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({Comentario:txtArea})
-    });
-    // const comment = await enviarComentario.json();
-    
-    
+async function sendComment(txtArea) {
+  return await fetch('http://localhost:3000/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ Comentario: txtArea })
+  });
+  // const comment = await enviarComentario.json();
+
+
 }
